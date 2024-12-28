5712b18ed5e860d58761b3858439ff304cda989e
vaibha987 <v2goyal95@gmail.com>
2024-12-28 12:59:09 +0530

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router"
+const Home=()=>{
+return(
+<>
+<Link to={"/"}>Home</Link>
+<Link to={"/about"}>About</Link>
+<h1>Welcome to my website!</h1>
+
+</>
+
+)
+}
+
+export default Home
